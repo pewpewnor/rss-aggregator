935f3d739d3f9e9d0e8c5ae83b40d2925fd01a4e
pewpewnor <newpatchnotworking2@tutanota.com>
2023-08-15 14:58:45 +0700

    refactor: limit lines to 80 char

--- src/handler_user.go
+++ src/handler_user.go
@@ -15,7 +15,8 @@ func (hc *HandlerContext) handleCreateUser(c *gin.Context) {
 	}
 
 	if err := c.ShouldBindJSON(&params); err != nil {
-		c.JSON(400, res.SimpleErrorResponseFromError("Request body is invalid", err))
+		c.JSON(400, res.SimpleErrorResponseFromError(
+			"Invalid request body", err))
 		return
 	}
 
@@ -26,15 +27,18 @@ func (hc *HandlerContext) handleCreateUser(c *gin.Context) {
 		Name:      params.Name,
 	})
 	if err != nil {
-		c.JSON(500, res.SimpleErrorResponseFromError("Cannot create user in database", err))
+		c.JSON(500, res.SimpleErrorResponseFromError(
+			"Cannot create user in database", err))
 		return
 	}
 
-	c.JSON(201, res.SuccessResponse(gin.H{"user": dbUserToModelUser(user)}, "User successfully created"))
+	c.JSON(201, res.SuccessResponse(
+		gin.H{"user": dbUserToModelUser(user)}, "User successfully created"))
 }
 
 func (hc *HandlerContext) handleGetUser(c *gin.Context) {
 	user, _ := c.MustGet("user").(User)
 
-	c.JSON(200, res.SuccessResponse(gin.H{"user": user}, "User successfully found"))
+	c.JSON(200, res.SuccessResponse(
+		gin.H{"user": user}, "User successfully found"))
 }
