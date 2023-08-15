081301dcff397246bb5d71e988fa8d43b912b431
pewpewnor <newpatchnotworking2@tutanota.com>
2023-08-15 16:26:04 +0700

    feat: add alive middleware

---
+++ src/handler/middleware_alive.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pewpewnor/rss-aggregator/src/res"
+)
+
+func (hc *HandlerContext) AliveMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, err := hc.DB.Alive(c)
+
+		if err == nil {
+			c.AbortWithStatusJSON(500, res.SimpleErrorResponseFromError(
+				"Server down / inactive", err))
+			return
+		}
+
+		c.Next()
+	}
+}
