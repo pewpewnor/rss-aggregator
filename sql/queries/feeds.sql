f198e0e9cce482e02a7029bf9f22a211fbd79b36
pewpewnor <newpatchnotworking2@tutanota.com>
2023-08-16 15:07:08 +0700

    feat: add last_fetch_at column

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -2,3 +2,14 @@
 INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE ID = $1
+RETURNING *;
